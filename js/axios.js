5ee23e835aa8440615587251cfb42b6f2a233b36
BlueAbe <andrzej.jan.jaworski@gmail.com>
2022-07-10 19:48:08 +0200

    changes in mail-sender

--- js/axios.js
+++ js/axios.js
@@ -3,7 +3,6 @@ const sub_1 = document.querySelector(".sub-1");
 const sub_1__ico = document.querySelector(".sub-1__ico");
 const sub_1__text = document.querySelector(".sub-1__text");
 
-// type is either 'password' or 'data'
 const updateSettings = async (data) => {
   try {
     const url = "https://applications.com.pl/api/v1/mail-sender";
@@ -38,12 +37,16 @@ if (dataForm)
   dataForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const form = new FormData();
-    form.append("name", document.getElementById("name").value);
-    form.append("sender", document.getElementById("email").value);
-    form.append("message", document.getElementById("message").value);
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    const messageTemplate = `<h1 style="background-color:#e4b668; max-width:1000px; margin:0 auto; padding:30px 0px; font-size:26px; font-family: Athiti, sans-serif; color: white; text-align:center"><span style="display:block">Świetnie !</span>Nowa wiadomość nadesłana przez osobę przedstawiającą się jako ${name}.</h1>
+<h2 style="font-size: 20px; padding:40px 0px; font-family: Athiti, sans-serif; text-align:center">Na otrzymaną wiadomość należy odpisać pod adres <a href="mailto:${email}" target="_blank">${email}</a></h2>
+<p style="font-family: Athiti, sans-serif; color:white;font-size:18px; background-color: #82a1b5; max-width:1000px; margin:0 auto; padding: 20px 40px; box-sizing:border-box">${message}</p>`;
+    form.append("message", messageTemplate);
     form.append("from", "Villa Rowena");
-    form.append("subject", "Villa Rowena - wiadomość z formularza");
+    form.append("subject", "Wiadomość z formularza");
     form.append("receiver", "villa.rowena@onet.pl");
-    // form.append("receiver", "andrzej.jan.jaworski@gmail.com");
+    form.append("receiver", "andrzej.jan.jaworski@gmail.com");
     updateSettings(form);
   });
